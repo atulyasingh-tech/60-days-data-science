30d8e183a1e39b34fdcb1dadb639642d45b3713d
Atulya Kumar Singh <atulyasingh57@gmail.com>
2026-09-05 19:23:43 +0530

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,10 @@
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+sal=int(input("enter monthly salary: "))
+year_sal=sal*12
+employee = {
+    "Name": name,
+    "Age": age,
+    "Monthly Salary": sal,
+    "Yearly Salary": yearly_sal
+}
